87e0c1087b64e285bd9f9110493e78f0b43fa664
Jakub Jawor <jj94@op.pl>
2018-05-22 20:07:24 +0200

    modify the controller's methods, use JPA repository to findAll, findById, getOne and delete

--- src/main/java/com/example/book/controller/BookController.java
+++ src/main/java/com/example/book/controller/BookController.java
@@ -1,6 +1,7 @@
 package com.example.book.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,15 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.book.entity.Book;
-import com.example.book.service.BookService;
+import com.example.book.repository.BookRepository;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
 	@Autowired
-	BookService bookService;
-
+	//BookService bookService;
+	BookRepository bookRepo;
 	@RequestMapping("/hello")
 	public String hello() {
 		return "asdfasdf";
@@ -28,24 +29,30 @@ public class BookController {
 
 	@GetMapping("/")
 	public List<Book> getBookList() {
-		return bookService.getList();
+		//return bookService.getList();
+		return bookRepo.findAll();
 	}
 	
 	@GetMapping("/{id}")
-	public Book getBookById(@PathVariable long id){
-		return bookService.getById(id);
+	public Optional<Book> getBookById(@PathVariable long id){
+		//return bookService.getById(id);
+		return bookRepo.findById(id);
+		
 	}
 	
 	@PostMapping("/")
 	public Book addBook(@RequestBody Book book){
-		bookService.addBook(book);
+		//bookService.addBook(book);
+		bookRepo.save(book);
 		return new Book();
 	}
 	
 	@DeleteMapping("/{id}")
 	public String deleteBook(@PathVariable long id){
-		Book book = bookService.getById(id);
-		bookService.deleteBook(book);
+		//Book book = bookService.getById(id);
+		//bookService.deleteBook(book);
+		Book book = bookRepo.getOne(id);
+		bookRepo.delete(book);
 		return "Delete completed";
 	}
 
